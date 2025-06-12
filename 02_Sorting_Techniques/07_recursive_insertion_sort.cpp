35258beeace9496aa991e5042be76db3ee634770
tanishqtote55 <tanishqtote.55@gmail.com>
2025-06-12 15:17:02 +0530

    7. Recursive Insertion Sort Code Added

---
+++ 02_Sorting_Techniques/07_recursive_insertion_sort.cpp
@@ -0,0 +1,54 @@
+/*
+    Problem Statement: Given an array of N integers, write a program to implement the Recursive Insertion Sort algorithm.
+
+    Examples:
+
+    Example 1:
+    Input: N = 6, array[] = {13,46,24,52,20,9}
+    Output: 9,13,20,24,46,52
+    Explanation: After sorting we get 9,13,20,24,46,52
+
+    Example 2:
+    Input: N = 5, array[] = {5,4,3,2,1}
+    Output: 1,2,3,4,5
+    Explanation: After sorting we get 1,2,3,4,5
+*/
+
+#include <iostream>
+using namespace std;
+
+void insertion_sort(int arr[], int i, int n) {
+
+    // Base Case: i == n.
+    if (i == n) return;
+
+    int j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+        int temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+        j--;
+    }
+
+    insertion_sort(arr, i + 1, n);
+}
+
+int main()
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Using insertion Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    insertion_sort(arr, 0, n);
+    cout << "After Using insertion sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+}
