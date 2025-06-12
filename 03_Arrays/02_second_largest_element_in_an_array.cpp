e1356726752074e7c73fe90bb9039f19db97c8e3
tanishqtote55 <tanishqtote.55@gmail.com>
2025-06-12 18:41:31 +0530

    2. Second Largest Element in an Array Code Added

---
+++ 03_Arrays/02_second_largest_element_in_an_array.cpp
@@ -0,0 +1,76 @@
+/*
+    Second Largest Element
+    Given an array of integers nums, return the second-largest element in the array. If the second-largest element does not exist, return -1.
+
+    Examples:
+    Input: nums = [8, 8, 7, 6, 5]
+
+    Output: 7
+
+    Explanation: The largest value in nums is 8, the second largest is 7
+
+    Input: nums = [10, 10, 10, 10, 10]
+
+    Output: -1
+
+    Explanation: The only value in nums is 10, so there is no second largest value, thus -1 is returned
+*/
+
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    int secondLargestElement(vector<int>& nums) {
+        //your code goes here
+        if(nums.size() < 2) return -1;
+        int largest = INT_MIN;
+        int second_largest = INT_MIN;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] > largest){
+                second_largest = largest;
+                largest = nums[i];
+            }
+            else if (nums[i] > second_largest && nums[i] != largest){
+                second_largest = nums[i];
+            }
+        }    
+        return (second_largest == INT_MIN) ? -1 : second_largest;    
+    }
+    int secondSmallestElement(vector<int>& nums) {
+        if(nums.size() < 2) return -1;
+        int smallest = INT_MAX;
+        int second_smallest = INT_MAX;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] < smallest) {
+                second_smallest = smallest;
+                smallest = nums[i];
+            }
+            else if (nums[i] < second_smallest && nums[i] != smallest) {
+                second_smallest = nums[i];
+            }
+        }
+        return (second_smallest == INT_MAX) ? -1 : second_smallest;
+    }
+};
+
+int main() {
+    int arr[] = {1, 2, 4, 7, 7, 5};  
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    vector<int> nums(arr, arr + n);
+    Solution sol;
+
+    int sS = sol.secondSmallestElement(nums);
+    int sL = sol.secondLargestElement(nums);
+
+    cout << "Second smallest is " << sS << endl;
+    cout << "Second largest is " << sL << endl;
+
+    return 0;
+}
+
