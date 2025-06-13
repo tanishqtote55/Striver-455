830a49277416a97830c3072e29abb239eea059e9
tanishqtote55 <tanishqtote.55@gmail.com>
2025-06-13 17:15:43 +0530

    22. Rearrange Array Elements by Sign Leetcode Code Added

---
+++ 03_Arrays/22_rearrange_array_elements_by_sign_leetcode.cpp
@@ -0,0 +1,97 @@
+/*
+    2149. Rearrange Array Elements by Sign
+    You are given a 0-indexed integer array nums of even length consisting of an equal number of positive and negative integers.
+
+    You should return the array of nums such that the the array follows the given conditions:
+
+    Every consecutive pair of integers have opposite signs.
+    For all integers with the same sign, the order in which they were present in nums is preserved.
+    The rearranged array begins with a positive integer.
+    Return the modified array after rearranging the elements to satisfy the aforementioned conditions.    
+
+    Example 1:
+
+    Input: nums = [3,1,-2,-5,2,-4]
+    Output: [3,-2,1,-5,2,-4]
+    Explanation:
+    The positive integers in nums are [3,1,2]. The negative integers are [-2,-5,-4].
+    The only possible way to rearrange them such that they satisfy all conditions is [3,-2,1,-5,2,-4].
+    Other ways such as [1,-2,2,-5,3,-4], [3,1,2,-2,-5,-4], [-2,3,-5,1,-4,2] are incorrect because they do not satisfy one or more conditions.  
+    Example 2:
+
+    Input: nums = [-1,1]
+    Output: [1,-1]
+    Explanation:
+    1 is the only positive integer and -1 the only negative integer in nums.
+    So nums is rearranged to [1,-1].
+    
+    Constraints:
+
+    2 <= nums.length <= 2 * 105
+    nums.length is even
+    1 <= |nums[i]| <= 105
+    nums consists of equal number of positive and negative integers.
+
+    It is not required to do the modifications in-place.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    // vector<int> rearrangeArray(vector<int>& nums) {
+    //     vector<int> negative, positive;
+    //     for(int i = 0; i < nums.size(); i++){
+    //         if(nums[i] < 0){
+    //             negative.push_back(nums[i]);
+    //         } else {
+    //             positive.push_back(nums[i]);
+    //         }
+    //     }   
+    //     int posIdx = 0;
+    //     int negIdx = 0;
+    //     for(int i = 0; i < nums.size(); i++){
+    //         if(i % 2 == 0){
+    //             nums[i] = positive[posIdx++];
+    //         } else {
+    //             nums[i] = negative[negIdx++];
+    //         }
+    //     }
+    //     return nums;
+    // }
+
+
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+        int posIdx = 0; 
+        int negIdx = 1; 
+        for(int i = 0; i < n; i++) {
+            if (nums[i] >= 0) { 
+                result[posIdx] = nums[i];
+                posIdx += 2;
+            } else { 
+                result[negIdx] = nums[i];
+                negIdx += 2;
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    
+    Solution sol;
+    vector<int> result = sol.rearrangeArray(arr);
+    
+    for(int i : result) cout << i << " "; cout << '\n';
+
+    return 0;
+}
+
