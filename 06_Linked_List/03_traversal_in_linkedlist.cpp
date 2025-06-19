6534281ed151080355900090bdf7f502be8b72f7
tanishqtote55 <tanishqtote.55@gmail.com>
2025-06-19 14:58:43 +0530

    3. Traversal in Linked List Code Added

---
+++ 06_Linked_List/03_traversal_in_linkedlist.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Node{
+    public: // access modifier
+    int data; // the data value
+    Node* next; // the pointer to the next value
+    public:
+    // constructor
+    Node (int data1, Node* next1){
+        data=data1;  // Initialize data with the provided value
+        next=next1;  // Initialize next with the provided
+    }
+    Node (int data1){
+        data=data1;  // Initialize data with the provided value
+        next=nullptr;  // Initialize next as null since it's the end of the list
+
+    }
+};
+
+Node* convertArrToLL(vector<int>& arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 1; i < arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover -> next = temp;
+        mover = temp; // mover = mover -> next;
+    }
+    return head;
+}
+
+int main(){
+    vector<int> arr = {2, 5, 8, 7};
+    Node* head = convertArrToLL(arr);
+
+    // Traversal in Linked List
+    Node* temp = head;
+    while(temp){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    
+    return 0;
+}
