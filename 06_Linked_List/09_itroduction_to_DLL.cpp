4acb3c7a370898cd3d8e8a759730a12430655bc3
tanishqtote55 <tanishqtote.55@gmail.com>
2025-06-19 18:43:06 +0530

    9. Introduction to DLL Code Added

---
+++ 06_Linked_List/09_itroduction_to_DLL.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Node {
+public:
+    int data;       // Data stored in the node
+    Node* next;     // Pointer to the next node in the list (forward direction)
+    Node* back;     // Pointer to the previous node in the list (backward direction)
+
+    // Constructor for a Node with both data, a reference to the next node, and a reference to the previous node
+    Node(int data1, Node* next1, Node* back1) {
+        data = data1;
+        next = next1; // Similar to a singly linked list, but now with a 'back' pointer.
+        back = back1; // Addition of 'back' pointer for the doubly linked list.
+    }
+
+    // Constructor for a Node with data, and no references to the next and previous nodes (end of the list)
+    Node(int data1) {
+        data = data1;
+        next = nullptr; // Similar to a singly linked list.
+        back = nullptr; // Addition of 'back' pointer initialization.
+    }
+};
+
+Node* convertArrToDLL(vector<int> arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i = 1; i < arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev -> next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void print(Node* head){
+    Node* temp = head;
+    while(temp){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+}
+
+int main(){
+    vector<int> arr = {2, 5, 8, 7};
+    Node* head = convertArrToDLL(arr);
+    print(head);
+    return 0;
+}
