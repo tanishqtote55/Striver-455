5e3f5db513bb0947df6b6ca81ae639e92a31734e
tanishqtote55 <tanishqtote.55@gmail.com>
2025-06-22 13:58:07 +0530

    9. All Pattersn in Print All Subsequences Code Added

--- 07_Recursion/09_print_all_subsequences_with_sum_k.cpp
+++ 07_Recursion/09_print_all_subsequences_with_sum_k.cpp
@@ -4,22 +4,93 @@ using namespace std;
 
 class Solution {
 public:
-    void printS(int index, vector<int> &ds, int s, int sum, vector<int> arr, int n){
+    // Print All Subsequences
+    // void printS(int index, vector<int> &ds, int s, int sum, vector<int> arr, int n){
+    //     if(index == n){
+    //         if(s == sum){
+    //             for(auto it: ds) cout << it << " ";
+    //             cout << endl;
+    //         }
+    //         return;
+    //     } 
+    //     // take or pick the particular index into the subsequence
+    //     ds.push_back(arr[index]);
+    //     s += arr[index];
+    //     printS(index + 1, ds, s, sum, arr, n);
+    //     ds.pop_back();
+    //     // not take or not pick the particular index into the subsequence
+    //     s -= arr[index];
+    //     printS(index + 1, ds, s, sum, arr, n);
+    // }
+
+    // Print Any Subsequences using Boolean Flag
+    // bool flag = false;
+    // void printS(int index, vector<int> &ds, int s, int sum, vector<int> arr, int n){
+    //     if(index == n){
+    //         if(s == sum && flag == false){
+    //             flag = true;
+    //             for(auto it: ds) cout << it << " ";
+    //             cout << endl;
+    //         }
+    //         return;
+    //     } 
+    //     // take or pick the particular index into the subsequence
+    //     ds.push_back(arr[index]);
+    //     s += arr[index];
+    //     printS(index + 1, ds, s, sum, arr, n);
+    //     ds.pop_back();
+    //     // not take or not pick the particular index into the subsequence
+    //     s -= arr[index];
+    //     printS(index + 1, ds, s, sum, arr, n);
+    // }
+
+    // Print Any Subsequences using Boolean Function
+    // bool printS(int index, vector<int> &ds, int s, int sum, vector<int> arr, int n){
+    //     if(index == n){
+    //         // Condition Satisfied
+    //         if(s == sum){
+    //             for(auto it: ds) cout << it << " ";
+    //             cout << endl;
+    //             return true;
+    //         }
+    //         // Condition Not Satisfied
+    //         else return false;;
+    //     } 
+    //     // take or pick the particular index into the subsequence
+    //     ds.push_back(arr[index]);
+    //     s += arr[index];
+    //     if(printS(index + 1, ds, s, sum, arr, n) == true){
+    //         return true;
+    //     }
+    //     ds.pop_back();
+    //     // not take or not pick the particular index into the subsequence
+    //     s -= arr[index];
+    //     if(printS(index + 1, ds, s, sum, arr, n) == true ){
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    // Count the Number of Subsequences  
+    int printS(int index, vector<int> &ds, int s, int sum, vector<int> arr, int n){
+        // Condition not satisfied
+        // strictly done if array contains positives only
+        if(s > sum) return 0;
         if(index == n){
-            if(s == sum){
-                for(auto it: ds) cout << it << " ";
-                cout << endl;
-            }
-            return;
+            // Condition Satisfied
+            if(s == sum) return 1;
+            // Condition Not Satisfied
+            else return 0;
         } 
         // take or pick the particular index into the subsequence
         ds.push_back(arr[index]);
         s += arr[index];
-        printS(index + 1, ds, s, sum, arr, n);
+        int l = printS(index + 1, ds, s, sum, arr, n);
         ds.pop_back();
         // not take or not pick the particular index into the subsequence
         s -= arr[index];
-        printS(index + 1, ds, s, sum, arr, n);
+        int r = printS(index + 1, ds, s, sum, arr, n);
+        return l + r;
     }
 };
 
@@ -30,7 +101,8 @@ int main() {
     int n = 3;
     int sum = 2;
     vector<int> ds;
-    sol.printS(0, ds, 0, sum, nums, n);
+    // sol.printS(0, ds, 0, sum, nums, n);
+    cout << sol.printS(0, ds, 0, sum, nums, n);
 
     return 0;
 }
