f58c941209d2c8633ec1c03d7bd423b6ae78defe
tanishqtote55 <tanishqtote.55@gmail.com>
2025-06-25 13:59:18 +0530

    3. Implement Stack Using Linked List Question Updated

--- 09_Stack_and_Queues/03_Implement_Stack_using_LinkedList.cpp
+++ 09_Stack_and_Queues/03_Implement_Stack_using_LinkedList.cpp
@@ -1,5 +1,33 @@
 /*
+    Implement stack using Linkedlist
+    Implement a Last-In-First-Out (LIFO) stack using a singly linked list. The implemented stack should support the following operations: push, pop, top, and isEmpty.
 
+    Implement the LinkedListStack class:
+    void push(int x): Pushes element x onto the stack.
+    int pop(): Removes and returns the top element of the stack.
+    int top(): Returns the top element of the stack without removing it.
+    boolean isEmpty(): Returns true if the stack is empty, false otherwise.
+
+    Examples:
+    Input:
+    ["LinkedListStack", "push", "push", "pop", "top", "isEmpty"]
+    [[], [3], [7], [], [], []]
+    Output: [null, null, null, 7, 3, false]
+    Explanation:
+    LinkedListStack stack = new LinkedListStack();
+    stack.push(3);
+    stack.push(7);
+    stack.pop(); // returns 7
+    stack.top(); // returns 3
+    stack.isEmpty(); // returns false
+
+    Input:
+    ["LinkedListStack", "isEmpty"]
+    [[]]
+    Output: [null, true]
+    Explanation:
+    LinkedListStack stack = new LinkedListStack();
+    stack.isEmpty(); // returns true
 */
 
 #include <iostream>
