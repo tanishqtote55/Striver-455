25bf3f343f6c8f54e7dbce59b301e64082bfa9bc
tanishqtote55 <tanishqtote.55@gmail.com>
2025-06-29 12:59:34 +0530

    17. Find Median From Data Stream Leetcode Code Added

---
+++ 11_Heaps/17_Find_Median_From_Data_Stream_Leetcode.cpp
@@ -0,0 +1,91 @@
+/*
+    295. Find Median from Data Stream
+    The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+
+    For example, for arr = [2,3,4], the median is 3.
+    For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+    Implement the MedianFinder class:
+
+    MedianFinder() initializes the MedianFinder object.
+    void addNum(int num) adds the integer num from the data stream to the data structure.
+    double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+    
+    Example 1:
+
+    Input
+    ["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+    [[], [1], [2], [], [3], []]
+    Output
+    [null, null, null, 1.5, null, 2.0]
+
+    Explanation
+    MedianFinder medianFinder = new MedianFinder();
+    medianFinder.addNum(1);    // arr = [1]
+    medianFinder.addNum(2);    // arr = [1, 2]
+    medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+    medianFinder.addNum(3);    // arr[1, 2, 3]
+    medianFinder.findMedian(); // return 2.0
+    
+    Constraints:
+
+    -105 <= num <= 105
+    There will be at least one element in the data structure before calling findMedian.
+    At most 5 * 104 calls will be made to addNum and findMedian.
+
+    Follow up:
+    
+    If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+    If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+*/
+   
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+class MedianFinder {
+private:
+    priority_queue<int> maxHeap; // left side: max-heap
+    priority_queue<int, vector<int>, greater<int>> minHeap; // right side: min-heap
+
+public:
+    MedianFinder() {}
+
+    void addNum(int num) {
+        // Step 1: Add to maxHeap
+        maxHeap.push(num);
+
+        // Step 2: Move top of maxHeap to minHeap (balance halves)
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+
+        // Step 3: Rebalance (maxHeap can have one more element than minHeap)
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+
+    double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.top() + minHeap.top()) / 2.0;
+        }
+        return maxHeap.top(); // if odd, maxHeap has the extra element
+    }
+};
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
+
+int main() {
+    MedianFinder mf;
+    mf.addNum(1);
+    mf.addNum(2);
+    cout << "Median: " << mf.findMedian() << endl; // 1.5
+    mf.addNum(3);
+    cout << "Median: " << mf.findMedian() << endl; // 2.0
+    return 0;
+}
