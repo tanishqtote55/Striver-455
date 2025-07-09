1f806017eecb6dd581b2079cd155637c108e5ca4
tanishqtote55 <tanishqtote.55@gmail.com>
2025-07-09 11:21:08 +0530

    07. Number Of Islands Code Updated With DFS Logic

--- 15_Graphs/07_Number_Of_Islands.cpp
+++ 15_Graphs/07_Number_Of_Islands.cpp
@@ -9,6 +9,28 @@ using namespace std;
 
 class Solution {
 public:
+    void dfs(int row, int col, vector<vector<char>> &grid, vector<vector<bool>> &visited) {
+        int n = grid.size();
+        int m = grid[0].size();
+        visited[row][col] = true;
+
+        // 8-direction movement
+        int drow[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int dcol[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        for (int i = 0; i < 8; ++i) {
+            int newRow = row + drow[i];
+            int newCol = col + dcol[i];
+
+            // Check bounds and unvisited land
+            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m) {
+                if (grid[newRow][newCol] == '1' && !visited[newRow][newCol]) {
+                    dfs(newRow, newCol, grid, visited);
+                }
+            }
+        }
+    }
+
     void bfs(int startRow, int startCol, vector<vector<char>> &grid, vector<vector<bool>> &visited) {
         int n = grid.size();
         int m = grid[0].size();
@@ -52,6 +74,7 @@ public:
             for (int col = 0; col < m; ++col) {
                 if (grid[row][col] == '1' && !visited[row][col]) {
                     bfs(row, col, grid, visited);
+                    // dfs(row, col, grid, visited);
                     count++;
                 }
             }
